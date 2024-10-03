a596225e5e524fea1ae1b4d62d0504fedc7b7b11
clint <diamosclint@gmail.com>
2024-10-03 03:54:48 -0700

    1003_Vid20_SQL Injection Vulnerabilities Explained

--- Database.php
+++ Database.php
@@ -4,25 +4,20 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        //tutorial code
-        //$dsn = "mysql:host=localhost;port=3306;dbname=appnew;user=root;charset=utf8mb4";
-        //$this->connection = new PDO($dsn);
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $dsn = "mysql:host=localhost;port=3306;dbname=appnew;user=root;charset=utf8mb4";
-
-        $username = "root"; // my MySQL username
-        $password = "ucbanilad"; // my MySQL password
-
-        $this->connection = new PDO($dsn, $username, $password);
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
-    public function query($query)
+    public function query($query, $params = [])
     {
         $statement = $this->connection->prepare($query);
 
-        $statement->execute();
+        $statement->execute($params);
 
         return $statement;
     }
