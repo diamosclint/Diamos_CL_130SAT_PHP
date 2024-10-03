4b162dcbbae758ab9ee3659f2ad12624d6f326e2
clint <diamosclint@gmail.com>
2024-10-03 07:29:19 -0700

    1003_Vid25_Queues Are Easier Than You Think

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('test',function(){
-    //\Illuminate\Support\Facades\Mail::to('info@example.com')->send(
-        //new \App\Mail\JobPosted()
-    //);
+Route::get('test', function () {
+    $job = Job::first();
 
-    //return 'Done';
-//});
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
