f2ea3ce460d55c2f103ae94c4b01404d5400a4cb
clint <diamosclint@gmail.com>
2024-10-03 03:35:52 -0700

    1003_Vid17_PDO First Steps

--- index.php
+++ index.php
@@ -2,3 +2,17 @@
 
 require 'functions.php';
 require 'router.php';
+
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts where id = 1");
+$statement->execute();
+
+$post = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
