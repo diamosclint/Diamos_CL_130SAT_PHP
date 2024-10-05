af6f586bee0790dbc3a11494b2d25f59a13aac8e
clint <diamosclint@gmail.com>
2024-10-05 05:20:53 -0700

    1005_Vid31_Namespacing: What, Why, How?

--- Core/Database.php
+++ Core/Database.php
@@ -1,16 +1,20 @@
 <?php
 
+namespace Core;
+
+use PDO;
+
 class Database
 {
     public $connection;
     public $statement;
 
-    public function __construct($config, $username = 'root', $password = 'ucbanilad')
+    public function __construct($config, $username = 'root', $password = '')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
 
