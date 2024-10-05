fc801bb9ca351015a4de2a03bc3619d4a6cd8e7a
clint <diamosclint@gmail.com>
2024-10-05 04:44:43 -0700

    1005_Vid30_PHP Autoloading and Extraction

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,25 +1,17 @@
 <?php
 
-require 'Validator.php';
-
-$config = require 'config.php';
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = 'Create Note';
+$currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
 
-    if (! Validator::string($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1,000 characters is required.';
-    }
+authorize($note['user_id'] === $currentUserId);
 
-    if (empty($errors)) {
-        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
-}
-
-require 'views/notes/create.view.php';
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
